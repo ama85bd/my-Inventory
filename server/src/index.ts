0202928ba8c1b5065faacd21fef85c3d56746f2b
asifm.ashique <asifm.ashique@petronas.com>
2025-04-22 00:00:12 +0600

    expense

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,7 @@ import morgan from 'morgan';
 import dashboardRoutes from './routes/dashboardRoutes';
 import productRoutes from './routes/productRoutes';
 import userRoutes from './routes/userRoutes';
+import expenseRoutes from './routes/expenseRoutes';
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -23,10 +24,10 @@ app.use(cors());
 
 /* ROUTES */
 app.use('/dashboard', dashboardRoutes); // http://localhost:8000/dashboard
-
 app.use('/products', productRoutes); // http://localhost:8000/products
-
 app.use('/users', userRoutes); // http://localhost:8000/users
+app.use('/expenses', expenseRoutes); // http://localhost:8000/expenses
+
 /* SERVER */
 const port = Number(process.env.PORT) || 3001;
 app.listen(port, '0.0.0.0', () => {
